bc705c7981126c487065dfb7f7e3ae533bd16d48
Masataro Asai <guicho2.71828@gmail.com>
2014-07-12 08:36:53 +0900

    refactored the test script

--- t/a-star.lisp
+++ t/a-star.lisp
@@ -10,91 +10,71 @@ Copyright (c) 2013 guicho ()
 
 ;; blah blah blah.
 
-(defvar *max* 300)
-(defparameter *sample-num* 55)
-(defparameter *samples* nil)
-(defparameter *edges* nil)
-(defparameter *max-edges-per-node* 3)
+(defvar *max* 300 "The dimension of square field")
 
-(defun make-samples ()
-  (setf *samples* nil)
-  (iter (repeat *sample-num*)
-        (push (2d (random *max*) (random *max*))
-              *samples*)))
+(defun make-samples (sample-num max)
+  "make random samples"
+  (iter (repeat sample-num)
+        (collect (2d (random max) (random max)))))
 
-(defun connect-neighbors ()
-  (setf *edges* nil)
-  (iter (for 2d in *samples*)
-        (iter (generate i below (+ 2 (random 1)))
-              (for samples
-                   initially *samples*
-                   then (remove best samples))
-              (for best = 
-                   (iter (for sample in samples)
-                         (finding
-                          sample
-                          minimizing
-                          (heuristic-cost-between 2d sample))))
-              (unless best
-                (terminate))
-              (when (< (length (edges best)) *max-edges-per-node*)
-                (push (connect 2d best) *edges*)
-                (next i)))))
+(defun make-graph (samples average-edge-number)
+  (iter outer
+        (for 2d in samples)
+        (iter (for i below (floor
+                            (* average-edge-number
+                               (+ 1 (gaussian-random -1 1)))))
+              (in outer
+                  (collect
+                      (connect 2d (random-elt (remove 2d samples))))))))
 
-(test a*
-  (make-samples)
-  (connect-neighbors)
-  (finishes
+(test test-with-draw
+  (let* ((samples (make-samples 500 *max*))
+         (edges (make-graph samples 10)))
     (with-canvas (:width *max* :height *max*)
       (set-rgba-stroke 0 0 0 0.3)
       (set-rgba-fill 0 0 0 0.3)
       (set-line-width 1)
-      (mapcar #'draw *samples*)
-      (mapcar #'draw *edges*)
+      (mapc #'draw samples)
+      (mapc #'draw edges)
       (unwind-protect
-          (let* ((start (random-elt *samples*))
-                 (end  (iter (for candidate = (random-elt *samples*))
-                             (if (eq start candidate)
-                                 (next-iteration)
-                                 (return candidate))))
-                 (last (a*-search start end)))
-            (with-graphics-state
-              (set-rgba-fill 1 0 0 0.7)
-              (set-rgba-stroke 1 0 0 0.7)
-              (draw start)
-              (draw end)
-              (set-rgba-fill 1 1 0 0.7)
-              (set-rgba-stroke 1 1 0 0.7)
-              (draw-path last)))
-	(ensure-directories-exist
-	 (asdf:system-relative-pathname
-	  :aflab1
-	  "result/"))
-	(save-png (asdf:system-relative-pathname
-		   :aflab1
-		   (format nil "result/~a.png" (now))))))))
+           (let* ((start (random-elt samples))
+                  (end  (iter (for candidate = (random-elt samples))
+                              (if (eq start candidate)
+                                  (next-iteration)
+                                  (return candidate))))
+                  (last (a*-search start end)))
+             (with-graphics-state
+               (set-rgba-fill 1 0 0 0.7)
+               (set-rgba-stroke 1 0 0 0.7)
+               (draw start)
+               (draw end)
+               (set-line-width 2)
+               (set-rgba-fill 1 0 0 0.5)
+               (set-rgba-stroke 1 0 0 0.5)
+               (draw-path last)))
+        (ensure-directories-exist
+         (asdf:system-relative-pathname
+          :aflab1
+          "result/"))
+        (save-png (asdf:system-relative-pathname
+                   :aflab1
+                   (format nil "result/~a.png" (now))))))))
 
 (defun benchmark ()
-  (let ((*max* 300)
-        (*sample-num* 55)
-        (*max-edges-per-node* 7))
-    (make-samples)
-    (connect-neighbors)
-    (let* ((start (random-elt *samples*))
-           (end  (iter (for candidate = (random-elt *samples*))
-                       (if (eq start candidate)
-                           (next-iteration)
-                           (return candidate)))))
+  (let* ((samples (make-samples 150 *max*))
+         (edges (make-graph samples 7)))
+    (format t "Number of samples: ~50A~&Number of edges: ~50A~&"
+            (length samples) (length edges))
+    (let* ((start (random-elt samples))
+           (end (random-elt (remove start samples))))
       (time
-       (%benchmark1 start end))
-      (time
-       (%benchmark2 start end)))))
+       (%benchmark1 start end)))))
 
-(defun %benchmark (start end)
+(defun %benchmark1 (start end)
   (declare (inline a*-search))
   (a*-search start end))
 
-(defun %benchmark (start end)
+(defun %benchmark2 (start end)
   (declare (inline a*-search))
   (a*-search start end))
 

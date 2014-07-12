e7967f510e8435af2f7585b282799b75d25d0ff8
Masataro Asai <guicho2.71828@gmail.com>
2014-07-12 16:30:26 +0900

    optimize length

--- src/priority-queue.lisp
+++ src/priority-queue.lisp
@@ -28,9 +28,15 @@
     (removef (red-black-node-content node) thing))
   queue)
 
+(declaim (ftype (function ((or red-black-node leaf)) fixnum) queue-length))
 @export
 (defun queue-length (tree)
+  (declare (optimize (debug 0) (safety 0) (space 0) (speed 3)))
   (match tree
     ((rb-node _ left _ content right)
-     (+ (queue-length left) (queue-length right) (length content)))
+     (the fixnum
+         ;;(declare (ftype (function (&rest fixnum) fixnum) +))
+         (+ (the fixnum (+ (queue-length left)
+                           (queue-length right)))
+            (the fixnum (length (the list content))))))
     ((leaf) 0)))

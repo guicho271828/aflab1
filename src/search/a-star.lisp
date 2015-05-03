fde07159c6e9ed4517ab49c83852cd1be8f51827
Masataro Asai <guicho2.71828@gmail.com>
2015-05-03 20:52:43 +0900

    bag is boxed

--- src/search/a-star.lisp
+++ src/search/a-star.lisp
@@ -6,6 +6,7 @@
    #:a-star-node
    #:a-star-node-g
    #:a-star-node-f
+   #:a-star-node-h
    #:a-star-node-status
    #:+closed+
    #:+open+
@@ -17,18 +18,23 @@
 (defconstant +neither+ -1)
 
 (defstruct (a-star-node (:include node))
-  (g MOST-POSITIVE-FIXNUM :type fixnum)
+  (g 0 :type fixnum)
   (f MOST-POSITIVE-FIXNUM :type fixnum)
+  (h -1 :type fixnum)
   (status +neither+ :type (integer -1 1)))
 
 (implement-interface
- (ea*.bag:bag-interface hash-table a-star-node)
- :inherit (hash-table node))
+ (ea*.bag:bag-interface ea*.bag.h:hash-bag a-star-node)
+ :inherit (ea*.bag.h:hash-bag node))
+(implement-interface
+ (ea*.bag:bag-interface ea*.bag.l:list-bag a-star-node)
+ :inherit (ea*.bag.l:list-bag node))
 
 (defpackage :eazy-a-star.search.a-star.eager
   (:use :cl :structure-interface :trivia
         :ea*.b :ea*.s.a :ea*.q)
   (:shadowing-import-from :immutable-struct :ftype)
+  (:shadow :init)
   (:nicknames :ea*.s.a.e))
 (in-package :ea*.s.a.e)
 
@@ -36,42 +42,55 @@
 
 (implement-interface (ea*.s:search-interface a-star-node))
 
-(defun expand (open closed h succ)
+(defun expand (open closed heuristic succ)
   (lambda (node)
     (declare (notinline delete-node enqueue))
-    (delete-node open (a-star-node-f node) node)
-    (enqueue closed (a-star-node-f node) node)
-    (map nil
-         (lambda-ematch
-           ((edge :cost c
-                  :to (and to (a-star-node :f (place f f1)
-                                           :g g
-                                           :status (place status)
-                                           :parent (place p))))
-            (let ((f2 (+ g c (funcall h to))))
-              (match* (f2 status)
-                ((_ +neither+)
-                 (setf f f2
-                       p node
-                       status +open+)
-                 (enqueue open f2 to))
-                (((< f1) +open+)
-                 (setf f f2
-                       p node)
-                 (delete-node open f1 to)
-                 (enqueue open f2 to))
-                (((< f1) +closed+)
-                 (setf f f2
-                       p node
-                       status +open+)
-                 (delete-node closed f1 to)
-                 (enqueue open f2 to))))))
-         (funcall succ node))
+    (ematch node
+      ((a-star-node :g g1 :f f1 :status (place status))
+       (setf status +closed+)
+       (with-open-file (*standard-output* "/dev/null" :direction :output :if-exists :overwrite)
+         (delete-node open f1 node)
+         (enqueue closed f1 node))
+       (map nil
+            (lambda-ematch
+              ((edge :cost c :to to)
+               (ematch to
+                 ((a-star-node :f (place f)
+                               :h (place h)
+                               :g (place g)
+                               :status (place status)
+                               :parent (place p))
+                  (when (minusp h) (setf h (funcall heuristic to)))
+                  (let* ((g2 (+ g1 c)))
+                    (match* (g2 status)
+                      ((_ +neither+)
+                       (setf f (+ g2 h)
+                             g g2
+                             p node
+                             status +open+)
+                       (enqueue open f to))
+                      (((< g) +open+)
+                       (delete-node open f to)
+                       (setf f (+ g2 h)
+                             g g2
+                             p node)
+                       (enqueue open f to))
+                      (((< g) +closed+)
+                       (delete-node closed f to)
+                       (setf f (+ g2 h)
+                             g g2
+                             p node
+                             status +open+)
+                       (enqueue open f to))))))))
+            (funcall succ node))))
     (values)))
 
 (defun fetch (open)
   (lambda ()
-    (dequeue open)))
+    (match (dequeue open)
+      ((and it (a-star-node f))
+       (format t "~&current minimum: ~a" f)
+       it))))
 
 
 ;;; lazy a*
